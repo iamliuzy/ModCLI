250c9b7944a01f4fdf7f0bf1585466d9a40c33e1
iamliuzy <liuzhiyu.sh@outlook.com>
2022-08-24 19:16:14 +0800

    Update main.py
    添加-add参数和-version(-ver)参数，暂未添加实际用途。

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import argparse
+import pathlib
 
 import lang
+import constants
 
 
 class ModCLI:
     lang_parse = lang.Lang()
+
     def argparster(self):
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--add", "-a", help=self.lang_parse.translate("help.add"))
-        args = parser.parse_args()
-        print(args.echo)
+        parster = argparse.ArgumentParser()
+        parster.add_argument("-add", help=self.lang_parse.translate("help.add"), type=pathlib.Path)
+        parster.add_argument("-version", "-ver", help=self.lang_parse.translate("help.version"), action="version", version="{name} {ver}".format(name=constants.NAME, ver=constants.VERSION))
+        args = parster.parse_args()
+        print(args.add)
 
     def __init__(self):
         self.argparster()
