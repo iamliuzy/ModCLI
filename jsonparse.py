63db60117206a81f49a381efcb5d1542242c889c
iamliuzy <liuzhiyu.sh@outlook.com>
2022-08-24 22:19:21 +0800

    Update jsonparse.py
    更新以适配Pykson模块API

--- jsonparse.py
+++ jsonparse.py
@@ -1,46 +1,39 @@
-import json
-import pathlib
-import logging
+from pathlib import Path
+from pykson import Pykson
 
+def _open(path:Path):
+    path = path.resolve()
+    file = open(path, "r", encoding="utf-8")
+    content = file.read()
+    file.close()
+    return content
 
-class Json:
-    """
-    Json API for this program.
-    """
+def _write(path:Path, content:str):
+    path = path.resolve()
+    file = open(path, "w", encoding="utf-8")
+    file.write(content)
+    file.close()
 
-    def __init__(self, is_list=False, path=pathlib.PurePath(".\\config.json")):
-        """
-        is_list: If this json file is a list, it should be "True".
-        path: The path of the json file.
-        """
-        self.is_List = is_list
+def json_to_object(json:str, pojo):
+    return Pykson.from_json(json, pojo)
 
-        self.path = path
+def file_to_object(json:Path, pojo):
+    return json_to_object(_open(json), pojo)
 
-        if not pathlib.Path(str(self.path)).exists():
-            logging.warning("No such json file '" + str(self.path) + ".")
-            raise FileNotFoundError("No such json file '" + str(self.path) + "'.")
+def object_to_json(pojo):
+    return Pykson.to_json(pojo)
 
-        logging.info("Loading json file '" + str(path) + "'.")
-        self.file = open(str(self.path), "r", encoding="utf-8")
-        self.pure_content = self.file.read()
-        logging.info("Load successful.")
-        if self.is_List:
-            self.content = list(json.loads(self.pure_content))
-        else:
-            self.content = dict(json.loads(self.pure_content))
-        self.file.close()
+def object_to_file(json:Path, pojo):
+    _write(json, object_to_json(pojo))
 
-    def parse(self) -> list | dict:
-        self.file.close()
-        return self.content
+def json_to_dict(json:str):
+    return json_to_object(json, dict)
 
-    def merge(self, content):
-        self.content = content
-        self.save()
-        return True
+def file_to_dict(json:Path):
+    return file_to_object(json, dict)
 
-    def save(self):
-        self.file = open(str(self.path), "w")
-        self.file.write(str(self.content))
-        self.file.close()
+def json_to_list(json:str):
+    return json_to_object(json, list)
+
+def file_to_list(json:Path):
+    return file_to_object(json, list)
