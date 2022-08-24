c1b420e89b4d8779de21a6a954747a7b6aff6280
iamliuzy <liuzhiyu.sh@outlook.com>
2022-08-24 18:13:37 +0800

    修改本地化翻译并添加测试参数

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import argparse
 
+import lang
+
 
 class CMOD:
-
+    lang_parse = lang.Lang()
     def argparster(self):
         parser = argparse.ArgumentParser()
-        parser.add_argument("--add", "-a", help=lang.)
+        parser.add_argument("--add", "-a", help=self.lang_parse.translate("help.add"))
         args = parser.parse_args()
         print(args.echo)
 
