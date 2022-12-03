d3064f92703b02479ea1b9fc5ef3a22213b2d7c8
iamliuzy <liuzhiyu.sh@outlook.com>
2022-12-03 17:22:18 +0800

    测试mods模块

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pathlib
 
 import lang
 import constants
+import mods
 
 
 class ModCLI:
@@ -14,10 +15,12 @@ class ModCLI:
         parster.add_argument("--version", "-v",
                              help=self.lang_parse.translate("help.version"), action="version",
                              version="{name} {ver}".format(name=constants.NAME, ver=constants.VERSION))
-        args = parster.parse_args()
+        self.args = vars(parster.parse_args())
 
     def __init__(self):
         self.argparster()
+        if not self.args["add"] == None:
+            mods.Mod(self.args["add"])
 
 
 if __name__ == '__main__':
